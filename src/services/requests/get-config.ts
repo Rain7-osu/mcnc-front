f793c017829a023831a17bfe865c77e1844ff96e
wbn <1789446861@qq.com>
2022-06-18 19:20:01 +0800

    v0.11.2 添加配置

--- src/services/requests/get-config.ts
+++ src/services/requests/get-config.ts
@@ -1,7 +1,8 @@
-import { request } from '../core/http';
+import { request, transformResponse } from '../core/http';
 import { transformConfig } from '../tools/transform-config';
 
 export const getConfig = async () => {
   const res = await request.get('/api/config');
-  return transformConfig(res);
+  const data = transformResponse(res);
+  return transformConfig(data);
 };
