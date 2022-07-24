31423c6f34fd584eaf4d59983bb50c2c7cc03cbe
wbn <1789446861@qq.com>
2022-07-24 15:19:23 +0800

    feat: 添加 tryout文件上传

---
+++ src/components/UploadButton/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const DocumentItem = styled.div`
+  margin: 15px 0;
+  
+  .document-title {
+    color: #ffffff;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+`;
+
+export const DocumentListContainer = styled.div`
+  width: 100%;
+  margin-top: 25px;
+`;
+
+export const SheetListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  .sheet-item {
+    margin: 2px 0;
+
+    a {
+      color: hsl(333, 40%, 80%);
+    }
+  }
+`;
